b598f8b6f596b287f72192f043f82dc9a8e2e5c1
Johnny So <jso123450@gmail.com>
2024-03-23 19:28:58 -0400

    fix: updated dir name in scripts

--- wordpress-compose/scripts/replace-domain.sh
+++ wordpress-compose/scripts/replace-domain.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 set -a
-. ./components/.env
+. ./wordpress-compose/.env
 set +a
 
 # replace the placeholder <DOMAIN> string with the DOMAIN value from .env for http config
-sed -i 's/<DOMAIN>/'"$DOMAIN"'/g' components/nginx-http/nginx.conf
+sed -i 's/<DOMAIN>/'"$DOMAIN"'/g' wordpress-compose/nginx-http/nginx.conf
 
 # replace the placeholder <DOMAIN> string with the DOMAIN value from .env for https config
-sed -i 's/<DOMAIN>/'"$DOMAIN"'/g' components/nginx-https/nginx.conf
+sed -i 's/<DOMAIN>/'"$DOMAIN"'/g' wordpress-compose/nginx-https/nginx.conf
 
 # uncomment the domain server_name directive
-sed -i 's/^    # /    /g' components/nginx-http/nginx.conf
+sed -i 's/^    # /    /g' wordpress-compose/nginx-http/nginx.conf
--- wordpress-compose/scripts/replace-ip.sh
+++ wordpress-compose/scripts/replace-ip.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 set -a
-. ./components/.env
+. ./wordpress-compose/.env
 set +a
 
 # replace the placeholder <IP> string with the IP value from .env for http config
-sed -i 's/<IP>/'"$IP"'/g' components/nginx-http/nginx.conf
+sed -i 's/<IP>/'"$IP"'/g' wordpress-compose/nginx-http/nginx.conf
 
 # replace the placeholder <IP> string with the IP value from .env for https config
-sed -i 's/<IP>/'"$IP"'/g' components/nginx-https/nginx.conf
+sed -i 's/<IP>/'"$IP"'/g' wordpress-compose/nginx-https/nginx.conf
